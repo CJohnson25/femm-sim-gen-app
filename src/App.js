dd18eb4e208770ffd633eee2b554a347ebafedcd
Colby Johnson <colby.johnson25@gmail.com>
2021-04-27 09:34:55 -0400

    Started form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import 'fontsource-roboto';
+import { FormControlLabel, Switch } from '@material-ui/core';
 
 function App() {
   return (
@@ -7,6 +8,31 @@ function App() {
       <header className="App-header">
         <h1>FEMM Simulation Generator</h1>
       </header>
+
+        # Units - Toggle
+      <FormControlLabel
+        control={<Switch name="units" />}
+        label="Units"
+      />
+        # Magnet Width - text
+        # Magnet height - text
+        # Grade - dropdown
+        # halbach? - toggle
+      <FormControlLabel
+        control={<Switch  name="halbach" />}
+        label="Halbach?"
+      />
+        # halbach Width - text
+        # halbach height - text 
+        # halbach Grade - dropdown
+        # back iron - toggle
+      <FormControlLabel
+        control={<Switch  name="backIron" />}
+        label="Back Iron?"
+      />
+        # back iron height - text
+        # air gap - text
+
     </div>
   );
 }
