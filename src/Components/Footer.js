7c97f089a76e8fe148f6a1290139819d300bfab3
Colby Johnson <colby.johnson25@gmail.com>
2021-04-28 23:55:01 -0400

    missed a few files

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,21 +4,21 @@ import pjson from '../../package.json'
 import { Grid, IconButton, Typography } from '@material-ui/core';
 
 export function Footer() {
-    return (
-        <Grid container style={{ marginTop: 40 }} justify="flex-end">
-            <Grid item xs={8}>
-                <Typography>Version: {pjson.version}</Typography>
-            </Grid>
-            <Grid item xs={1} >
-                <IconButton target="_blank" variant="contained" color="primary" href="https://www.youtube.com/channel/UCQk0CkSexTb7GQvpGxj4kxw">
-                    <YouTubeIcon />
-                </IconButton>
-            </Grid>
-            <Grid item xs={1} >
-                <IconButton target="_blank" variant="contained" color="primary" href="https://github.com/CJohnson25/femm-sim-gen-app/">
-                    <GitHubIcon />
-                </IconButton>
-            </Grid>
-        </Grid>
-    )
+  return (
+    <Grid container style={{ marginTop: 40 }} justify="flex-end">
+      <Grid item xs={8}>
+        <Typography>Version: {pjson.version}</Typography>
+      </Grid>
+      <Grid item xs={1} >
+        <IconButton target="_blank" variant="contained" color="primary" href="https://www.youtube.com/channel/UCQk0CkSexTb7GQvpGxj4kxw">
+          <YouTubeIcon />
+        </IconButton>
+      </Grid>
+      <Grid item xs={1} >
+        <IconButton target="_blank" variant="contained" color="primary" href="https://github.com/CJohnson25/femm-sim-gen-app/">
+          <GitHubIcon />
+        </IconButton>
+      </Grid>
+    </Grid>
+  )
 }
